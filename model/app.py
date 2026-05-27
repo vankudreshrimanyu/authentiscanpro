62d98f422091929d62dc18fa96108fb3fabbd4ff
Shrimanyu Vankudre <vankudreshrimanyu@gmail.com>
2026-05-28 03:09:15 +0530

    Updated Flask host

--- model/app.py
+++ model/app.py
@@ -133,4 +133,4 @@ def predict():
 
 if __name__ == "__main__":
     print("🌐 Starting Flask server on http://127.0.0.1:5000")
-    app.run(host="127.0.0.1", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
