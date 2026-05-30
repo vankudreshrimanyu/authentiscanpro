0357268e2454cbfe0668dc1037e399a034483d70
Shrimanyu Vankudre <vankudreshrimanyu@gmail.com>
2026-05-31 01:10:47 +0530

    Added Vercel Speed Insights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -7,5 +8,6 @@ createRoot(document.getElementById("root")!).render(
   <>
     <App />
     <Analytics />
+    <SpeedInsights />
   </>
 );
